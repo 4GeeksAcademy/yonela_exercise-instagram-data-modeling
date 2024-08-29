6e4d3c26d39b538288500df892a5e70f18c8367a
Yonela Carbajal Valdivia <94427904+YONELA12345@users.noreply.github.com>
2024-08-29 04:56:36 +0000

    Models Insta Yonela

--- src/models.py
+++ src/models.py
@@ -1,37 +1,59 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    firstname = Column(String(50))
+    lastname = Column(String(50))
+    email = Column(String(100), unique=True, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    posts = relationship('Post', backref='user', lazy=True)
+    comments = relationship('Comment', backref='author', lazy=True)
+    following = relationship('Follower', foreign_keys='Follower.user_from_id', backref='follower', lazy='dynamic')
+    followers = relationship('Follower', foreign_keys='Follower.user_to_id', backref='followed', lazy='dynamic')
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    
+    comments = relationship('Comment', backref='post', lazy=True)
+    media = relationship('Media', backref='post', lazy=True)
 
-    def to_dict(self):
-        return {}
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum('image', 'video', 'audio', name='media_type'))
+    url = Column(String(255), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
-## Draw from SQLAlchemy base
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(255), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+def to_dict(self):
+    return {}
+
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
